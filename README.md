835e29dbd2e04568a34a97c4f29efd9d22c60afe
Stefan Claas <sacenator@gmail.com>
2019-12-25 14:13:55 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# adiantum
+
+[Adiantum Encryption](https://security.googleblog.com/2019/02/introducing-adiantum-encryption-for.html)
+
+This is a modified version of [ilius's CLI version](https://github.com/ilius/adiantum-cli) so that Windows users can use the software as well.
+
+Modifications where made for key and nonce usage, so that users can load a key file instead of typing in a passphrase and a nonce file when encrypting or decrypting messages.
+
+Nonce usage requires that for each encrypted message a new nonce has to be used.
+
+Nonces should be at least 12 bytes in size.
+
+A key must be 32 bytes in size!
+
+$ adiantum -h, adiantum --help
+
+### Usage of adiantum:
+
+### adiantum [-d] keyfile noncefile < infile > outfile
---
+++ adiantum.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"lukechampine.com/adiantum"
+)
+
+func encryptFromStdin() {
+	input, _ := ioutil.ReadAll(os.Stdin)
+	if len(input) == 0 {
+		return
+	}
+	noNewline := input[len(input)-1] != '\n'
+	input = bytes.TrimRight(input, "\n")
+	if len(input) < 16 {
+		zeros := bytes.Repeat([]byte{0}, 16-len(input))
+		input = append(input, zeros...)
+	}
+	secretkey, err := ioutil.ReadFile(flag.Arg(0))
+	if err != nil {
+		panic(err)
+	}
+	nonce, err := ioutil.ReadFile(flag.Arg(1))
+	if err != nil {
+		panic(err)
+	}
+	keyInput := []byte(secretkey)
+	if len(keyInput) != 32 {
+		panic(fmt.Errorf("Your key is %d bytes in size. Exactly 32 bytes are required.", len(keyInput)))
+	}
+	key := secretkey
+	tweak := nonce // can be any length, but should be at least 12 bytes.
+	cipher := adiantum.New(key)
+	output := cipher.Encrypt(input, tweak)
+	if noNewline {
+		fmt.Print(string(output))
+	} else {
+		fmt.Println(string(output))
+	}
+}
+
+func decryptFromStdin() {
+	input, _ := ioutil.ReadAll(os.Stdin)
+	if len(input) == 0 {
+		return
+	}
+	noNewline := input[len(input)-1] != '\n'
+	input = bytes.TrimRight(input, "\n")
+	if len(input) < 16 {
+		zeros := bytes.Repeat([]byte{0}, 16-len(input))
+		input = append(input, zeros...)
+	}
+	secretkey, err := ioutil.ReadFile(flag.Arg(0))
+	if err != nil {
+		panic(err)
+	}
+	nonce, err := ioutil.ReadFile(flag.Arg(1))
+	if err != nil {
+		panic(err)
+	}	
+	keyInput := []byte(secretkey)
+	if len(keyInput) != 32 {
+		panic(fmt.Errorf("Your key is %d bytes in size. Exactly 32 bytes are required.", len(keyInput)))
+	}
+	key := secretkey
+	tweak := nonce // can be any length, but should be at least 12 bytes.
+	cipher := adiantum.New(key)
+	output := cipher.Decrypt(input, tweak)
+	if noNewline {
+		fmt.Print(string(output))
+	} else {
+		fmt.Println(string(output))
+	}
+}
+
+func main() {
+
+	decryptFlag := flag.Bool("d", false, "Decrypt: adiantum -d keyfile noncefile < infile > outfile")
+
+	flag.Parse()
+	
+	if decryptFlag != nil && *decryptFlag {
+		decryptFromStdin()
+	} else {
+		encryptFromStdin()
+	}
+}
